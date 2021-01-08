c039f83ce9b569ae9f5aed83510217bec995f9df
David Levy <lennox.davidlevy@gmail.com>
2021-01-07 23:04:19 -0500

    Chapter 4 complete

---
+++ lists/buffet.py
@@ -0,0 +1,8 @@
+buffet_items = ("ice cream", "salad", "chicken sticks", "dumplings", "roast beef")
+print("Original items on the buffet")
+for item in buffet_items:
+    print(item)
+print("\nNew items on the buffet")
+buffet_items = ("ice cream", "jello", "steak sticks", "dumplings", "roast beef")
+for item in buffet_items:
+    print(item)
---
+++ lists/dimensions.py
@@ -0,0 +1,10 @@
+dimensions = (200, 50)
+print(f"Original dimensions")
+print(dimensions)
+for dimension in dimensions:
+    print(dimension)
+print("\n")
+dimensions = (400, 1000)
+print(f"New dimensions")
+for dimension in dimensions:
+    print(dimension)
